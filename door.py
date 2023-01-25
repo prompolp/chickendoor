c3c00dedc645476d61cbf1120f6ad5dc153d2f99
George <you@example.com>
2023-01-25 11:48:42 -0500

    Light Refactoring

--- door.py
+++ door.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 Python source code - replace this with a description of the code and write the
@@ -7,24 +7,29 @@ code below this text.
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-import door
 import RPi.GPIO as GPIO
 import datetime
 import time
 
-FORWARD = 11
-REVERSE = 13
-REED_OPEN = 19
-REED_CLOSE = 15
-LOCK_ENGAGE = 9
+PIN_MOTOR_FORWARD = 11
+PIN_MOTOR_REVERSE = 13
+PIN_SWITCH_DOOROPEN_POSITION = 19
+PIN_SWITCH_DOORCLOSE_POSITION = 15
+
+# Set the LOCK_ENGAGE_TIME to zero when there is no locking mechinisim.
+LOCK_ENGAGE_TIME = 0
 
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(FORWARD, GPIO.OUT)
-    GPIO.setup(REVERSE, GPIO.OUT)
-    GPIO.setup(REED_OPEN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(REED_CLOSE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    # Define Motor Pins
+    GPIO.setup(PIN_MOTOR_FORWARD, GPIO.OUT)
+    GPIO.setup(PIN_MOTOR_REVERSE, GPIO.OUT)
+
+    # Define Swich pins
+    GPIO.setup(PIN_SWITCH_DOOROPEN_POSITION, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(PIN_SWITCH_DOORCLOSE_POSITION, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 def cleanup():
@@ -32,44 +37,48 @@ def cleanup():
 
 
 def open():
-    if door.isOpen():
+    # if door is alread open do nothing.
+    if isOpen():
         return
-    print '%s : DOOR Opening...' % datetime.datetime.now()
-    GPIO.output(FORWARD, GPIO.HIGH)
-    while GPIO.input(REED_OPEN):
+
+    print ('%s : DOOR Opening...') % datetime.datetime.now()
+
+    GPIO.output(PIN_MOTOR_FORWARD, GPIO.HIGH)
+    while GPIO.input(PIN_SWITCH_DOOROPEN_POSITION):
         time.sleep(1.5)
-    print '%s : DOOR Opened!' % datetime.datetime.now()
-    GPIO.output(FORWARD, GPIO.LOW)
+    GPIO.output(PIN_MOTOR_FORWARD, GPIO.LOW)
 
-
-def isOpen():
-    if not GPIO.input(REED_OPEN):
-        print '%s : DOOR Open!' % datetime.datetime.now()
-
-
-def forward(seconds):
-    GPIO.output(FORWARD, GPIO.HIGH)
-    time.sleep(seconds)
-    GPIO.output(FORWARD, GPIO.LOW)
+    print ('%s : DOOR Opened!') % datetime.datetime.now()
 
 
 def close():
-    if door.isClosed():
+    # if door is alread closed do nothing.
+    if not isOpen():
         return
-    print '%s : DOOR Closing...' % datetime.datetime.now()
-    GPIO.output(REVERSE, GPIO.HIGH)
-    while GPIO.input(REED_CLOSE):
-        time.sleep(LOCK_ENGAGE)
-    print '%s : DOOR Closed!' % datetime.datetime.now()
-    GPIO.output(REVERSE, GPIO.LOW)
+
+    print ('%s : DOOR Closing...') % datetime.datetime.now()
+
+    GPIO.output(PIN_MOTOR_REVERSE, GPIO.HIGH)
+    while GPIO.input(PIN_SWITCH_DOORCLOSE_POSITION):
+        time.sleep(LOCK_ENGAGE_TIME)
+    GPIO.output(PIN_MOTOR_REVERSE, GPIO.LOW)
+
+    print ('%s : DOOR Closed!') % datetime.datetime.now()
 
 
-def isClosed():
-    if not GPIO.input(REED_CLOSE):
-        print '%s : DOOR Closed!' % datetime.datetime.now()
+def forward(seconds):
+    GPIO.output(PIN_MOTOR_FORWARD, GPIO.HIGH)
+    time.sleep(seconds)
+    GPIO.output(PIN_MOTOR_FORWARD, GPIO.LOW)
 
 
 def reverse(seconds):
-    GPIO.output(REVERSE, GPIO.HIGH)
+    GPIO.output(PIN_MOTOR_REVERSE, GPIO.HIGH)
     time.sleep(seconds)
-    GPIO.output(REVERSE, GPIO.LOW)
+    GPIO.output(PIN_MOTOR_REVERSE, GPIO.LOW)
+
+
+def isOpen():
+    if GPIO.input(PIN_SWITCH_DOOROPEN_POSITION):
+        return True
+    return False
